db5465d24365f8097514dc7065b4deff5dcc6ff3
parasnadar <pkp805326@gmail.com>
2026-07-28 15:34:59 +0530

    order report menu

--- src/app/Shared/menus/report/order-report/order-report.component.ts
+++ src/app/Shared/menus/report/order-report/order-report.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Select } from 'primeng/select';
+import { DatePickerModule } from 'primeng/datepicker';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-order-report',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    Select,
+    DatePickerModule,
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+  ],
   templateUrl: './order-report.component.html',
   styleUrl: './order-report.component.scss',
 })
 export class OrderReportComponent {
+  constructor(private fb: FormBuilder) {}
+  orderReport!: FormGroup;
 
+  ngOnInit(): void {
+    this.orderReport = this.fb.group({
+      courtNo: ['1'],
+
+      listingDate: [null],
+    });
+  }
+
+  courtOption = [
+    { label: 'Court I', value: '1' },
+    { label: 'Court II', value: '2' },
+    { label: 'Court III', value: '3' },
+    { label: 'Court IV', value: '4' },
+  ];
+
+  onSearch(): void {
+    console.log('Executing query payload:', this.orderReport.value);
+  }
+
+  onReset(): void {
+    this.orderReport.patchValue({
+      courtNo: '1',
+      diaryFilingNo: '',
+      listingDate: null,
+      purpose: '1',
+    });
+  }
 }
